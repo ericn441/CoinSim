d65642640d2435e6bef4ec1eaf7a7e260007be0a
Eric Ngo <ericn441@gmail.com>
2018-01-10 17:04:01 -0600

    Initial Commit

---
+++ CoinSim/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CoinSim
+//
+//  Created by Eric Ngo - 1 on 1/10/18.
+//  Copyright © 2018 Eric Ngo - 1. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
