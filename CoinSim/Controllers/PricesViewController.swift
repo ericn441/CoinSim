1a7dc9aa9641426cb62d13d7bbee2d53344d429a
Eric Ngo <ericn441@gmail.com>
2018-01-10 20:59:15 -0600

    setup main price UI

--- CoinSim/Controllers/PricesViewController.swift
+++ CoinSim/Controllers/PricesViewController.swift
@@ -7,10 +7,55 @@
 //
 
 import UIKit
+import MXParallaxHeader
 
-class PricesViewController: UIViewController {
-
+class PricesViewController: UIViewController, UITableViewDelegate, UITableViewDataSource, MXParallaxHeaderDelegate {
+    
+    @IBOutlet var headerView: UIView!
+    @IBOutlet weak var tableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        // Parallax Header
+        tableView.parallaxHeader.view = headerView // You can set the parallax header view from the floating view
+        tableView.parallaxHeader.height = 150
+        tableView.parallaxHeader.mode = MXParallaxHeaderMode.fill
+        tableView.parallaxHeader.delegate = self
     }
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        tableView.parallaxHeader.minimumHeight = topLayoutGuide.length
+    }
+    
+    //MARK: - UITableView Protocols
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 5
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "priceCell", for: indexPath) as! PricesTableViewCell
+        //Round card corners
+        cell.cardView.layer.cornerRadius = 5
+        cell.cardView.layer.masksToBounds = true
+        
+        //disable cell highlight
+        cell.selectionStyle = .none
+
+        return cell
+    }
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: false)
+    }
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 160
+    }
+}
+
+class PricesTableViewCell: UITableViewCell {
+    @IBOutlet weak var coinName: UILabel!
+    @IBOutlet weak var cardView: UIView!
+    
 }
--- Podfile
+++ Podfile
@@ -2,4 +2,5 @@ target 'CoinSim' do
     use_frameworks!
     pod 'Alamofire'
     pod 'SwiftyJSON'
+    pod 'MXParallaxHeader'
 end
